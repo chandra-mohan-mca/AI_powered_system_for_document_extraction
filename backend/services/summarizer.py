91e1d37d12e95410707e517bc4d3c9d0dcf15946
chandra-mohan-mca <chandru04c@gmail.com>
2026-04-04 19:52:59 +0530

    Update summarizer.py

--- backend/services/summarizer.py
+++ backend/services/summarizer.py
@@ -3,7 +3,7 @@ import os
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 def summarize_text(text: str) -> str:
     response = model.generate_content(f"Summarize this document:\n{text[:2000]}")
